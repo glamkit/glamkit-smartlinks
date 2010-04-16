b9a492cdcf73972712abea7e1b68bd44eee0488c
Thomas Ashelford <thomas@ether.com.au>
2010-04-16 14:55:16 -0600

    boilerplating

---
+++ README.rst
@@ -0,0 +1,9 @@
+==================
+GLAMkit-smartlinks
+==================
+
+Conditional links to Django models. For more information, see the `documentation <http://docs.glamkit.org/smartlinks/>`_.
+
+GLAMkit-smartlinks is part of the `GLAMkit framework <http://glamkit.org/>`_.
+
+Dependencies: None
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='glamkit-smartlinks',
+      version='1.0',
+      description='A template tag to help with concurrent media serving from Django',
+      author='Thomas Ashelford',
+      author_email='thomas@interaction.net.au',
+      url='http://glamkit.org/',
+      packages=['smartlinks',],
+     )
