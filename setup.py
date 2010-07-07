7a0304117f6a4ba3b27b96806e02010b84e9a1b3
Thomas Ashelford <thomas@ether.com.au>
2010-07-07 19:18:19 +1000

    Tidied up broken package

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='glamkit-smartlinks',
-      version='0.5.0',
-      description='A template tag to help with concurrent media serving from Django',
-      author='Thomas Ashelford',
-      author_email='thomas@interaction.net.au',
-      url='http://glamkit.org/',
-      packages=['smartlinks',],
-     )
+    version='0.5.1',
+    description='Conditional wiki-style links to Django models.',
+    author='Thomas Ashelford',
+    author_email='thomas@interaction.net.au',
+    url='http://glamkit.org/',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
