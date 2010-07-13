fb730b66a2538c7fc5e60895f9ef96f6f68b0eae
Thomas Ashelford <thomas@ether.com.au>
2010-07-13 13:12:27 +1000

    example settings now match docs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='glamkit-smartlinks',
-    version='0.6.0',
+    version='0.6.1',
     description='Conditional wiki-style links to Django models.',
     author='Thomas Ashelford',
     author_email='thomas@interaction.net.au',
